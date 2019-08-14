cc1f9f0fc55819de2bac4795e653bf4cc955eb4a
Joseph Luk <joluk@paypal.com>
2019-08-14 08:10:44 -0700

    Webpack.

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 const searchClient = algoliasearch(
-  'B1G2GM9NG0',
-  'aadef574be1f9252bb48d4ea09b5cfe5'
+  '7N37BK9GEW',
+  'c59028084f358d47fbf29f1548b6bcd2'
 );
 
 class App extends Component {
   render() {
     return (
       <div className="ais-InstantSearch">
-        <h1>React InstantSearch e-commerce demo</h1>
-        <InstantSearch indexName="demo_ecommerce" searchClient={searchClient}>
+        <h1>Everybody loves board games</h1>
+        <InstantSearch indexName="boardGames" searchClient={searchClient}>
           <div className="left-panel">
             <ClearRefinements />
             <h2>Brands</h2>
-            <RefinementList attribute="brand" />
+            <RefinementList attribute="category" />
             <Configure hitsPerPage={8} />
           </div>
           <div className="right-panel">
@@ -44,14 +44,14 @@ class App extends Component {
 function Hit(props) {
   return (
     <div>
-      <img src={props.hit.image} align="left" alt={props.hit.name} />
+      <img src={props.hit.image_url} align="left" alt={props.hit.names} />
       <div className="hit-name">
         <Highlight attribute="name" hit={props.hit} />
       </div>
       <div className="hit-description">
         <Highlight attribute="description" hit={props.hit} />
       </div>
-      <div className="hit-price">${props.hit.price}</div>
+      <div className="hit-price">Geek rating: {props.hit.geek_rating}</div>
     </div>
   );
 }
